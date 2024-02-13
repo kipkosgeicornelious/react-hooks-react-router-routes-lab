7870e4d7e0f3a36f7fd5a9ba9abb0bcd20f91b84
kipkosgeicornelious <kipkosgeicornelious6@gmail.com>
2024-02-13 10:45:31 +0300

    done

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,7 +2,21 @@ import React from "react";
 import { actors } from "../data";
 
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
+  return ( 
+  <div>
+    <h1>Actors Page</h1>
+    {actors.map((actor, Id) => (
+        <div key={Id}>
+          <h2>Name: {actor.name}</h2>
+          <ul>
+            {actor.movies.map((movie, movieId) => (
+              <li key={movieId}>{movie}</li>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </div>
+  )
 }
 
 export default Actors;
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,22 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div>
+      <h1>Movies Page</h1>
+      {movies.map((movie, Id) => (
+        <div key={Id}>
+          <h2>Title: {movie.title}</h2>
+          <p>Time: {movie.time} minutes</p>
+          <ul>
+            {movie.genres.map((genre, genreId) => (
+              <li key={genreId}>{genre}</li>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default Movies;
